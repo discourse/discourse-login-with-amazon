5aa7d50c0c94226ad6a1f7cf40abc2dbf9a4acce
David Taylor <david@taylorhq.com>
2022-12-29 10:15:11 +0000

    DEV: Introduce syntax_tree for ruby formatting

--- lib/validators/enable_login_with_amazon_validator.rb
+++ lib/validators/enable_login_with_amazon_validator.rb
@@ -6,11 +6,10 @@ class EnableLoginWithAmazonValidator
   end
 
   def valid_value?(val)
-    return true if val == 'f'
+    return true if val == "f"
 
     if SiteSetting.login_with_amazon_client_id.blank? ||
-       SiteSetting.login_with_amazon_client_secret.blank?
-
+         SiteSetting.login_with_amazon_client_secret.blank?
       return false
     end
 
@@ -19,9 +18,9 @@ class EnableLoginWithAmazonValidator
 
   def error_message
     if SiteSetting.login_with_amazon_client_id.blank?
-      I18n.t('site_settings.errors.login_with_amazon_client_id_is_blank')
+      I18n.t("site_settings.errors.login_with_amazon_client_id_is_blank")
     elsif SiteSetting.login_with_amazon_client_secret.blank?
-      I18n.t('site_settings.errors.login_with_amazon_client_secret_is_blank')
+      I18n.t("site_settings.errors.login_with_amazon_client_secret_is_blank")
     end
   end
 end
