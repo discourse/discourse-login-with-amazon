8100527147e2a0bc30b3e307eb73c807fcfbc012
marstall <marstall@gmail.com>
2024-02-12 11:51:51 -0500

    linting

--- lib/omniauth/strategies/amazon.rb
+++ lib/omniauth/strategies/amazon.rb
@@ -3,6 +3,7 @@
 # Will submit patch upstream for this change
 OmniAuth::Strategies::Amazon.class_eval do
   def callback_url
-    origin = ENV['REDIRECT_URL_ORIGIN']||full_host
-    origin + script_name + callback_path  end
+    origin = ENV["REDIRECT_URL_ORIGIN"] || full_host
+    origin + script_name + callback_path
+  end
 end
